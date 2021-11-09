8f0aefbd6d15e93ee18f30ad80710b2dddb353b7
Viliam Krizan <vkrizan@redhat.com>
2021-11-09 18:54:05 +0100

    Refactor integration package

--- splunk-quarkus/src/main/java/com/redhat/console/notifications/splunkintegration/SplunkIntegration.java
+++ splunk-quarkus/src/main/java/com/redhat/console/notifications/splunkintegration/SplunkIntegration.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.acme.timer.log;
+package com.redhat.cloud.notifications.splunkintegration;
 
 import org.apache.camel.builder.RouteBuilder;
 
