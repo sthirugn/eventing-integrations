47d23f6175dfc10aef8414f22a27b379507ded06
Viliam Krizan <vkrizan@redhat.com>
2022-08-26 13:26:37 +0200

    ci: push to old image during pr-check
    for smoother migration
    
    EVNT-613

--- pr_check.sh
+++ pr_check.sh
@@ -8,6 +8,7 @@
 APP_NAME="eventing"  # name of app-sre "application" folder this component lives in
 COMPONENT_NAME="eventing-splunk-quarkus"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
 IMAGE="quay.io/cloudservices/eventing-integrations"
+OLD_IMAGE="quay.io/cloudservices/eventing-integrations-splunk"
 DOCKERFILE="splunk-quarkus/Dockerfile.jvm"
 
 # Enviroment to take bonfire/clowdapp reources from
@@ -23,6 +24,13 @@ IQE_CJI_TIMEOUT="30m"
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
 curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 source $CICD_ROOT/build.sh
+
+# Compatiblity before migration
+set -x
+podman tag "${IMAGE}:${IMAGE_TAG}" "${OLD_IMAGE}:${IMAGE_TAG}"
+podman push "${OLD_IMAGE}:${IMAGE_TAG}"
+export IMAGE=$OLD_IMAGE
+set +x
 
 export COMPONENTS_W_RESOURCES="policies-engine"
 source $CICD_ROOT/deploy_ephemeral_env.sh
